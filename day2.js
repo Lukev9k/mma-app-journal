f6dd6957311d1d208726880f1a1fc054d67bbfe2
lukev9k <lukev09000@gmail.com>
2026-05-07 12:47:00 +0530

    wk1-day2: add day2 variable experiments

---
+++ day2.js
@@ -0,0 +1,14 @@
+const gymName = "Kinsou MMA"
+let studentCount = 12
+
+studentCount = 13
+
+console.log(gymName)
+console.log(studentCount)
+
+if (true) {
+    let insideMessage = "I am inside the block"
+    console.log(insideMessage)
+  }
+  
+  console.log(insideMessage)
